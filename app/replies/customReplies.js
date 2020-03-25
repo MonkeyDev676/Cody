10b448003cb21e5fec7995ea23b896d53dd5c5e1
MonkeyDev676 <61360216+MonkeyDev676@users.noreply.github.com>
2020-03-25 16:38:01 +0100

    Update customReplies.js

--- app/replies/customReplies.js
+++ app/replies/customReplies.js
@@ -1,13 +1,13 @@
 'use strict';
 module.exports = {
-  hi: ({ message }) => `Hi ${message.author.username}`,
-  'i am hungry': 'go to the shop and buy something to eat.',
-  'i love you': ['I love you too :D', 'aww! I also love you'],
-  'i am angry': 'Take rest and come to Discord. It make you to relax.',
-  'will you marry me': ['Our marriage hall will be at a cloud storage?', 'you sure abou tthat, darling?'],
+  hi: ({ message }) => `G'day ${message.author.username}`,
+  'im hunrgy': 'Mate, Just go the the store my man.',
+  'i love you': ['You are kinda thicc tho :D', 'Noice! I dont :/'],
+  'im angry': 'Boi, just get yo lazy ass. And relax a bit bro.',
+  'will you marry me': ['We can just be friends?', 'Sorry, I dont wanna friendzone you but lets be friends? '],
   'are you sleeping': ['No, dude.', 'Do I look like I am sleeping?'],
-  'are you lonely': ['Not Really', 'Not yet', 'I have amazing people like you to talk to.'],
+  'are you lonely': ['Not Really, Sometimes i guess?', 'Not as long as you are here.', 'I have amazing people like you to talk to.'],
   nothing: ['They why did u call me?', 'You are disturbing me'],
   'do you have burger': "No, i don't have. Sorry.",
-  'do you like me': 'Yeah, I like you.',
+  'do you like me': 'No sawwy.',
 };
