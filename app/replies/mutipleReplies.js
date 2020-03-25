681a57f7514f3ca74c3cfb7adbff693c8d0cdd6f
MonkeyDev676 <61360216+MonkeyDev676@users.noreply.github.com>
2020-03-25 16:28:29 +0100

    Update mutipleReplies.js

--- app/replies/mutipleReplies.js
+++ app/replies/mutipleReplies.js
@@ -1,5 +1,5 @@
 'use strict';
 module.exports = content => {
-  if (['who made you', 'who is your developer'].includes(content)) return 'an0Kx42, Itz Sudhan and Unique';
+  if (['who made you', 'who is your developer'].includes(content)) return 'Monkey';
   return null;
 };
